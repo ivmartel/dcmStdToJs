4ccfac4e397216a676e8e46cfe833f3a9c8a023e
ivmartel <ivmartel@gmail.com>
2020-10-21 22:35:30 +0200

    Update script to refer to this repo.

--- resources/scripts/update-gh-pages.sh
+++ resources/scripts/update-gh-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script to push current repo on the repository gh-pages branch.
 
-# we should be in /home/travis/build/ivmartel/dcmbench
+# we should be in /home/travis/build/ivmartel/dcmStdToJs
 echo -e "Starting to update gh-pages\n"
 
 # go to home and setup git
@@ -9,11 +9,11 @@ cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 # using token, clone gh-pages branch
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dcmbench.git gh-pages
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dcmStdToJs.git gh-pages
 # clean up demo
 rm -Rf $HOME/gh-pages/*
 # copy current repo in gh-pages
-cp -Rf $HOME/build/ivmartel/dcmbench/* $HOME/gh-pages/
+cp -Rf $HOME/build/ivmartel/dcmStdToJs/* $HOME/gh-pages/
 # add nojekyll file
 touch $HOME/gh-pages/.nojekyll
 # move back to root of repo
