a9f4e62357f9eeee12928f0e68146b447a03550e
ivmartel <ivmartel@gmail.com>
2021-05-04 19:17:19 +0200

    Add size info

--- src/gui.js
+++ src/gui.js
@@ -148,7 +148,10 @@ function appendResult(name, content) {
   if (typeof content.name !== 'undefined') {
     const para = document.createElement('p');
     para.appendChild(document.createTextNode(
-      content.name + ' (' + content.origin + ') '));
+      content.name +
+      ' (' + content.origin +
+      ', ' + content.raw.length + ' items) '
+    ));
 
     const link = document.createElement('a');
     link.download = 'result.json';
